22bd8bc220f0be82be684d8f92019c50d66870fc
Jon Martin <jchristofmartin@gmail.com>
2018-02-04 12:26:58 -0600

    added per-channel volume controlled by dial

--- src/controls/SampleChannel.tsx
+++ src/controls/SampleChannel.tsx
@@ -17,16 +17,22 @@ type State = {
 
 export default class SampleChannel extends React.Component<Props, State> {
     readonly bufferSource: AudioBufferSourceNode;
+    readonly channelGain: GainNode;
+
     audioBuffer: AudioBuffer;
+    volume = .5;
 
     constructor(props: Props) {
         super(props);
+        this.channelGain = props.audioSystem.audioContext().createGain();
+        this.channelGain.gain.value = this.volume;
+        this.channelGain.connect(this.props.audioSystem.channelInput());
       }
 
     render() {
         return (
             <div>
-            <Dial id={this.props.sampleName} onValueChanged={this.setVolume} dialSize={50} />
+            <Dial id={this.props.sampleName} onValueChanged={(id, value) => this.setVolume(id, value)} dialSize={50} />
             
             <SampleButton 
               clickFunction={
@@ -42,12 +48,15 @@ export default class SampleChannel extends React.Component<Props, State> {
         if (this.audioBuffer == null) {
             this.audioBuffer = this.props.audioLoader.audio(this.props.sampleName);
         }
+
+        this.channelGain.gain.value = this.volume;
         const bufferSource = this.props.audioSystem.bufferSource(this.audioBuffer);
-        bufferSource.connect(this.props.audioSystem.channelInput());
+        bufferSource.connect(this.channelGain);
         bufferSource.start();
     }
 
     private setVolume(id: string, value: number) {
+        this.volume = value;
         console.log('volume changed: ' + value);
     }
 }
