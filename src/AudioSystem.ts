50bb6fb72d8fc3fd38ee209f238be539d58a26c4
Jon Martin <jchristofmartin@gmail.com>
2018-02-11 11:37:15 -0600

    fix oscillator leaks

--- src/AudioSystem.ts
+++ src/AudioSystem.ts
@@ -3,18 +3,11 @@ import ConvolverReverb from './effects/ConvolverReverb';
 
 export default class  AudioSystem {
     context: AudioContext;
-    private oscillator: OscillatorNode;
     private gain: GainNode;
     
     constructor() {
-        this.context = new ((window as any).AudioContext || (window as any).webkitAudioContext)() as AudioContext;
-        this.oscillator = this.context.createOscillator();
-        
-        this.oscillator.type = 'sine';
-        this.oscillator.frequency.value = 440;
-        this.gain = this.context.createGain();
-        this.oscillator.connect(this.gain);
-        
+        this.context = new ((window as any).AudioContext || (window as any).webkitAudioContext)() as AudioContext;   
+        this.gain = this.context.createGain();   
         this.gain.connect(this.context.destination);
     }
 
@@ -46,21 +39,26 @@ export default class  AudioSystem {
         bufferSource.start();
     }
 
-    run(): void {
-        var now = (this.context as any).currentTime;
-        this.gain.gain.setValueAtTime(1, now);
-        this.gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5);
-        this.oscillator.start(now);
-        this.oscillator.stop(now + 0.5);
-    }
-
     note(freq: number) {
         const oscillator = this.context.createOscillator();
+        const gain = this.context.createGain();
+        gain.connect(this.gain);
+        gain.gain.value = 1;
         oscillator.frequency.value = freq;
-        oscillator.connect(this.gain);
+        oscillator.connect(gain);
         const now = (this.context as any).currentTime;
+
+        gain.gain.setTargetAtTime(0, now, .5);
+        
         oscillator.frequency.value = freq;
         oscillator.start(now);
-        oscillator.stop(now + 0.5);
+        window.setTimeout(
+            () => {
+                gain.disconnect();
+                oscillator.disconnect();
+                oscillator.stop();
+            }, 
+            6000);
+        // oscillator.stop(now + 1);
     }
 }
