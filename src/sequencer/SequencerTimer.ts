13b6aad9b72dc8297c6e92d805548e2794aeccfc
Jon Martin <jchristofmartin@gmail.com>
2018-02-25 18:17:24 -0600

    adding web worker to schedule audio events

--- src/sequencer/SequencerTimer.ts
+++ src/sequencer/SequencerTimer.ts
@@ -5,6 +5,7 @@ export default class SequencerTimer {
     private current16thNote = 0;
     private futureTickTime: number;
     private secondsPerBeat = 60 / this.tempo;
+    private timerWorker: any;
 
     private readonly audioSystem: AudioSystem;
     private timeoutHandle: number;
@@ -15,13 +16,16 @@ export default class SequencerTimer {
     constructor(audioSystem: AudioSystem) {
         this.audioSystem = audioSystem;
         this.futureTickTime = audioSystem.audioContext().currentTime;
+
+        this.timerWorker = new Worker('timerworker.js');
+        this.installworker();
+        this.timerWorker.postMessage('start');
         console.log(this.timeoutHandle);
     }
 
     public start(): void {
         console.log('start squencing');
         this.sequencing = true;
-        this.scheduler();
     }
 
     public stop(): void {
@@ -37,7 +41,22 @@ export default class SequencerTimer {
         return this._tempo;
     }
 
+    public installworker() {
+        this.timerWorker.onmessage = (e) => {
+            if (e.data === 'tick') {
+                console.log('tick!');
+                this.scheduler();
+            }
+            else
+                console.log('message: ' + e.data);
+        };
+        this.timerWorker.postMessage({'interval': 50});
+    }
+
     private scheduler(): void {
+        if (!this.sequencing)
+            return;
+
         while (this.futureTickTime < this.audioSystem.audioContext().currentTime + 0.1) {
             this.futureTickTime += 0.25 * this.secondsPerBeat;
             this.pulse(this.current16thNote, this.futureTickTime);
@@ -46,8 +65,5 @@ export default class SequencerTimer {
             if (this.current16thNote > 15)
                 this.current16thNote = 0;
         }
-
-        if (this.sequencing)
-            this.timeoutHandle = window.setTimeout(() => this.scheduler(),  50.0);
     }
 }
